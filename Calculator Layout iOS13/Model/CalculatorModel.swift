032a9d62f87408c716e4d0ca5b049e7921d96b0c
flyingking888 <jeremyfeiwang@outlook.com>
2024-03-07 17:25:04 -0500

    updated logic

---
+++ Calculator Layout iOS13/Model/CalculatorModel.swift
@@ -0,0 +1,9 @@
+//
+//  CalculatorModel.swift
+//  Calculator Layout iOS13
+//
+//  Created by Jeremy Wang on 3/5/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
